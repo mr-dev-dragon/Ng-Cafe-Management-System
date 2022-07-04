05cc5d2d8878ee169c201517d90f36851ac3c30d
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-07-04 17:36:49 +0100

    addd

--- frontend/ng-v14-project/src/app/app-routing.module.ts
+++ frontend/ng-v14-project/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/ng-v14-project/src/app/customer/customer-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer.component';
+
+const routes: Routes = [{ path: '', component: CustomerComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CustomerRoutingModule { }
