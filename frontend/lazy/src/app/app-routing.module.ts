4720013c80643b17a77af87696ce5768ce0b51c8
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-07-05 11:15:25 +0100

    add

--- frontend/lazy/src/app/app-routing.module.ts
+++ frontend/lazy/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AuthGuard } from './auth.guard';
+import { SearchComponent } from './search/search.component';
 const routes: Routes = [
   {
     path: 'pay',
@@ -12,7 +13,19 @@ const routes: Routes = [
     loadChildren: () => import('./payments/payments.module')
       .then(m => m.PaymentsModule)
   }
-];
+  ,
+  {
+    path: 'payments/success',
+    loadChildren: () => import('./payments/payments.module')
+      .then(m => m.PaymentsModule)
+    
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+    canActivate: [AuthGuard]
+  }
+];     
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/lazy/src/app/app.module.ts
+++ frontend/lazy/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchComponent,
 
   ],
   imports: [
