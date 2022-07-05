432841e6e7f1da3ddb0f2d03ba36a781d5de3a7d
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-07-05 14:58:45 +0100

    add btn dark mode

--- fg/fron/src/app/app.component.ts
+++ fg/fron/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'fron';
+  isDarkTheme = true;
+  btnnn() {
+    if (this.isDarkTheme) {
+      this.isDarkTheme = !this.isDarkTheme;
+    }
+
+  }
 }
